35b8b0ad377b3a2577c921b3b2a88535630d8e4a
surenderpal <surenderpal134@gmail.com>
2020-06-24 15:25:12 +0530

    able to click on hamburger menu

--- login.py
+++ login.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver=webdriver.Chrome()
 driver.get("http://ads-release-3-12-np.groundtruth.com/")
 time.sleep(5)
@@ -11,8 +14,8 @@ pwd.send_keys('Surenderpal@1991')
 signInButton=driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]')
 signInButton.click()
 
-
-TenantDD=driver.find_element_by_xpath(".//span[@class='dropdown-title ng-binding']").click()
-TenantSB=driver.find_element_by_xpath(".//input[@id='search-dropdown-list-Tenant']")
-TenantCkbox=driver.find_element_by_xpath(".//input[@name='Tenant-2']")
+# ---clicking on hamburger menu----
+button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
+button.click()
+time.sleep(5)
 driver.close()
