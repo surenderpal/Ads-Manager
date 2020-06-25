4f4a82a4715ef6778bdc97b6842f779b60e13c46
surenderpal <surenderpal134@gmail.com>
2020-06-25 17:56:15 +0530

    added top search bar

--- login.py
+++ login.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-# class Login:
-    # def __init__(url,email,pwd):
-    #     self.url=url
-    #     self.email=email
-    #     self.pwd=pwd
+from selenium.webdriver.support import expected_conditions as EC 
+driver=webdriver.Chrome()
+driver.get("http://ads-release-3-12-np.groundtruth.com/")
 
 def loginAdsManager():
-    driver=webdriver.Chrome()
-    driver.get("http://ads-release-3-12-np.groundtruth.com/")
+    # driver=webdriver.Chrome()
+    # driver.get("http://ads-release-3-12-np.groundtruth.com/")
+    time.sleep(2)
     email=driver.find_element_by_xpath('//*[@id="inp-signin-usernameLogin"]')
     email.send_keys('surender.pal@groundtruth.com')
     pwd=driver.find_element_by_xpath('//*[@id="inp-signin-passwordLogin"]')
@@ -27,7 +25,14 @@ def loginAdsManager():
     tenant_dashboard = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-tenant-dashboard")))
     tenant_dashboard.click()
 
-    time.sleep(3)
-    driver.close()  #closing browser
-# L=Login('http://ads-release-3-12-np.groundtruth.com','surender.pal@groundtruth.com','Surenderpal@1991')
+
+    # L=Login('http://ads-release-3-12-np.groundtruth.com','surender.pal@groundtruth.com','Surenderpal@1991')
+
+# ---search Ads Manager(ribbon search bar)---
+def change_tenant():
+    ribbon_search=WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".new-search-box-container #inp-base-searchbox-new")))
+    ribbon_search.click()
+    base_searchbox=driver.find_element_by_xpath("//input[@id='inp-base-searchbox-new']").send_keys('Test',Keys.RETURN)
+
 loginAdsManager()
+change_tenant()
