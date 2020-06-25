ebcf49d17e60bb8f1d2d5c122e74762fa3eb7a31
surenderpal <surenderpal134@gmail.com>
2020-06-25 12:17:39 +0530

    Inserted code into login method

--- login.py
+++ login.py
@@ -3,19 +3,32 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-driver=webdriver.Chrome()
-driver.get("http://ads-release-3-12-np.groundtruth.com/")
-time.sleep(5)
-email=driver.find_element_by_xpath('//*[@id="inp-signin-usernameLogin"]')
-email.send_keys('surender.pal@groundtruth.com')
-pwd=driver.find_element_by_xpath('//*[@id="inp-signin-passwordLogin"]')
-pwd.send_keys('Surenderpal@1991')
 
-signInButton=driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]')
-signInButton.click()
+# class Login:
+    # def __init__(url,email,pwd):
+    #     self.url=url
+    #     self.email=email
+    #     self.pwd=pwd
 
-# ---clicking on hamburger menu----
-button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
-button.click()
-time.sleep(5)
-driver.close()
+def loginAdsManager():
+    driver=webdriver.Chrome()
+    driver.get("http://ads-release-3-12-np.groundtruth.com/")
+    time.sleep(5)
+    email=driver.find_element_by_xpath('//*[@id="inp-signin-usernameLogin"]')
+    email.send_keys('surender.pal@groundtruth.com')
+    pwd=driver.find_element_by_xpath('//*[@id="inp-signin-passwordLogin"]')
+    pwd.send_keys('Surenderpal@1991')
+
+    signInButton=driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]')
+    signInButton.click()
+    # ---clicking on hamburger menu----
+    button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
+    button.click()
+    # ------clicking on Tenant Dashboard
+    tenant_dashboard = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-tenant-dashboard")))
+    tenant_dashboard.click()
+
+    time.sleep(5)
+    driver.close()  #closing browser
+    # L=Login()
+loginAdsManager()
