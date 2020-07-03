6c7b4255e6e22b5e6e8ad8eb668911ebf097c970
surenderpal <surenderpal134@gmail.com>
2020-07-04 00:02:49 +0530

    code to copy the Org Idfrom url

--- getUrl.py
+++ getUrl.py
@@ -1,8 +1,6 @@
 url='https://ads-release-3-13-np.groundtruth.com/organization/2021889?from_subsession=999c0fd0-c25d-48da-92a7-9b3b5c577911'
-count=1
 id=url.split("?")
 for ele in id:
-    print(ele,',',count)
-    count=count+1
+    print(ele)
     print(ele[-7:])
 # print()
