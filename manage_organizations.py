b38fb7296bc2156180b6fdb4c3ed368cee4c14bb
surenderpal <surenderpal134@gmail.com>
2020-07-04 00:02:05 +0530

    multiple accounts added

--- manage_organizations.py
+++ manage_organizations.py
@@ -19,22 +19,30 @@ def loginAdsManager():
     signInButton=driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]')
     signInButton.click()
 loginAdsManager()
+
+
 def NewOrg_Account():
     # ---clicking on hamburger menu----
-    hamburger = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
+    hamburger = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
     hamburger.click()
     # ----selecting  Manage Organization from hamburger------
-    org = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-manageOrg")))
+    org = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-manageOrg")))
     org.click()
     time.sleep(4)
-    driver.current_url() #URL containing organization id                                                                           
-    Organization_id=
     WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//button[@id='btn-orgList-newOrg']"))).click() #org model opens
     orgName=WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//input[@id='inp-newOrgModal-companyName']"))) #selects the org text box
-    orgName.send_keys("staging-3.1000") #enters the value in the text box.
-    driver.find_element_by_xpath("//button[@id='btn-newOrgModal-save']").click()
-    name=WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//input[@id='inp-newAccountModal-name']")))
-    name.send_keys('Test@123')
+    orgName.send_keys("staging-3.1") #enters the value in the text box.
+    driver.find_element_by_xpath("//button[@id='btn-newOrgModal-save']").click() #saving the form Organization
+    # driver.current_url() #URL containing organization id                                                                           
+    # Organization_id=2021889
+
+
+def accounts_model():
+    name=WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//input[@id='inp-newAccountModal-name']"))) #Name of company selected
+    
+    # ===========set Dynamic company name======================
+    
+    name.send_keys('Test') #Dynamic NAME SET                                                                  #value inserted into the company
     Advertiser_Domain=driver.find_element_by_xpath("//input[@id='inp-newAccountModal-adomain']") #advertiser Domain
     Advertiser_Domain.send_keys("GroundTruth.com")                                               #Value entered
     driver.find_element_by_xpath("//select[@id='inp-newAccountModal-countryCode']").click()      #Market dropdown clicked
@@ -48,8 +56,11 @@ def NewOrg_Account():
     driver.find_element_by_xpath("//select[@id='inp-newAccountModal-language']").click()         #Language dropdown
     driver.find_element_by_xpath("//select/option[text()='English']").click()
     driver.find_element_by_xpath("//select[@id='inp-newAccountModal-accountType']").click()      #Type Accounts
-    driver.find_element_by_xpath("//select/option[text()='Direct Client']").click()              #Direct Client
-        # driver.find_element_by_xpath("//select/option[text()='Direct Agency']").click()              #Direct Agency
+    
+    #==========set dynamic drop-down value===============
+
+    # driver.find_element_by_xpath("//select/option[text()='Direct Client']").click()              #Direct Client
+    driver.find_element_by_xpath("//select/option[text()='Direct Agency']").click()              #Direct Agency
         # driver.find_element_by_xpath("//select/option[text()='Platform CMR']").click()              #Platform CMR
         # driver.find_element_by_xpath("//select/option[text()='Platform Reseller']").click()              #Platform Reseller
         # driver.find_element_by_xpath("//select/option[text()='Platform Channel']").click()              #Platform Channel
@@ -61,15 +72,19 @@ def NewOrg_Account():
     driver.find_element_by_xpath("//button[@id='btn-newAccountModal-save']").click()
     time.sleep(5)
     # ---clicking on hamburger menu----
-    hamburger = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
+    hamburger = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".app-screen #btn-appMenu")))
     hamburger.click()
     # ----selecting  Manage Organization from hamburger------
-    org = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-manageOrg")))
+    org = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".app-screen #btn-appMenu-manageOrg")))
     org.click()
+    driver.find_element_by_xpath("//*[@id='tableBody-column-id']").click() # "//span[text()=2021894]" Now it is clicking on first row, but it has to click based on the index
+    time.sleep(2)
+    # WebDriverWait(driver,20).until()
+    driver.find_element_by_xpath("//button[@id='btn-orgEditModal-newAccount']").click()
+    time.sleep(2)
+
 NewOrg_Account()
-
-
-
-
+accounts_model()
+accounts_model()
 time.sleep(5)
 driver.close()
