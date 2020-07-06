e9668e996c1efefd84fbf6606598ba05978c2c27
surenderpal <surenderpal134@gmail.com>
2020-07-06 12:06:05 +0530

    Login page using Oops concept

---
+++ ads_manager_login.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support.select import Select
+class Ads_Manage_Login():
+    """ Ads manager login class"""
+    # staic variable
+    driver=webdriver.Chrome()
+    driver.get("http://ads-release-3-12-np.groundtruth.com/")
+    
+    def __init__(self,username,password): #constructor defined for dynamic username and password
+        self.username=username
+        self.password=password
+    
+    def login_ads(self):
+        email=self.driver.find_element_by_xpath('//*[@id="inp-signin-usernameLogin"]')
+        email.send_keys(self.username)
+        pwd=self.driver.find_element_by_xpath('//*[@id="inp-signin-passwordLogin"]')
+        pwd.send_keys(self.password)
+        signInButton=self.driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]')
+        signInButton.click()
+        print("{} has been logged in successfully!!".format(self.username))
+l=Ads_Manage_Login('surender.pal@groundtruth.com','Surenderpal@1991')
+l.login_ads()
