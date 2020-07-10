fe88d7ddd0fa5a6e66554d072e1c6b71af10c8d9
surenderpal <surenderpal134@gmail.com>
2020-07-10 17:35:13 +0530

    Campaign creation page till device completed

---
+++ campaign_creation.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver=webdriver.Chrome()
+driver.get("http://ads-release-3-13-np.groundtruth.com/")
+def Login(username,password):
+    email=driver.find_element_by_xpath('//*[@id="inp-signin-usernameLogin"]')
+    email.send_keys(username)
+    pwd=driver.find_element_by_xpath('//*[@id="inp-signin-passwordLogin"]')
+    pwd.send_keys(password)
+    driver.find_element_by_xpath('//*[@id="btn-signin-signIn"]').click()
+    time.sleep(3)
+
+def NewCampaign(name):
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH,"//button[@id='btn-campDash-newCampaign']"))).click() #clicked on new campaign
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH,"//input[@name='campaignNameField']"))).send_keys(name) #campaign name
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//input[@value='adgroup-budget']"))).click() #adgroup budget
+    
+    #====for campaign budget enter budget in the text box================ 
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//input[@value='campaign-budget']"))).click() #campaign budget
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='budgetField']"))).send_keys('5') # $ entered in campaign budget
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='btn-campaignCreateModal-submit']"))).click() #save button
+    time.sleep(4)
+
+def TargetingTactics(): #TTName--pased name as variable to select TTName
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//h3[text()='Target by Location']"))).click()  # Location TTName
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//h4[text()='On Premise Targeting']"))).click() #On Premise
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//h4[text()='Neighborhoods']"))).click() # Neighborhoods 
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//div/h4[text()='Geotargets']"))).click() # Geotargets 
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//h3[text()='Target by Audience']"))).click()  # Audience TTName
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//h3[text()='Target by Weather']"))).click()  # Weather TTName
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='btn-adgGoals-next']"))).click()  # Next Button
+    time.sleep(5)
+
+def DeviceType(type):
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, type))).click()
+    # WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//button/div[text()='CTV']"))).click()
+'''
+for mobile == //button/div[text()='Mobile']"
+for ctv== //button/div[text()='CTV']
+'''
+
+
+Login('gt.surender@protonmail.com','Groundtruth@9')
+NewCampaign('Automated campaign')
+TargetingTactics()
+DeviceType("//button/div[text()='Mobile']")
+driver.close()
