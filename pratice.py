cfe15a55e67a6d2655bafef3b914d17cea719841
surenderpal <surenderpal134@gmail.com>
2020-09-16 22:36:40 +0530

    working with links

--- pratice.py
+++ pratice.py
@@ -4,8 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
+from selenium .webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 import openpyxl
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager #for chrome
+from webdriver_manager.firefox import GeckoDriverManager #for firefox
+from webdriver_manager.microsoft import IEDriverManager #for IE
+from webdriver_manager.microsoft import EdgeChromiumDriverManager #for edge
+from time import sleep
+
 
 # driver=webdriver.Chrome()
 # driver.get('https://location-software-np.groundtruth.com/')
@@ -175,12 +184,51 @@ import openpyxl
 #     print()
 
 ## pratice --2 _------
-import openpyxl
-path='/Users/surenderpal/Downloads/Geotarget.xlsx'
-wb=openpyxl.load_workbook(path)
-sheet=wb['rahul']
-for r in range(1, 6):
-    for c in range(1,4):
-        sheet.cell(row=r,column=c).value='rahul'
-wb.save(path)
 
+# import openpyxl
+# path='/Users/surenderpal/Downloads/Geotarget.xlsx'
+# wb=openpyxl.load_workbook(path)
+# sheet=wb['rahul']
+# for r in range(1, 6):
+#     for c in range(1,4):
+#         sheet.cell(row=r,column=c).value='rahul'
+# wb.save(path)
+
+# pratice for links:
+# def browserName(bname):#bname
+bname='chrome'
+if bname=='chrome':
+    driver=webdriver.Chrome(ChromeDriverManager().install()) #this will install latest chrome driver manager
+elif bname=='firefox':
+    driver=webdriver.Firefox(executable_path=GeckoDriverManager().install()) #it will install latest firefox driver manager
+elif bname=='ie':
+    driver=webdriver.Ie(IEDriverManager().install())
+elif bname=='edge':
+    driver=webdriver.Edge(EdgeChromiumDriverManager().install())
+elif bname=='safari':
+    driver=webdriver.Safari()
+else:
+    print('Please pass the correct browser name' + bname)
+driver.get('https://ads-release-3-14-np.groundtruth.com/')
+# browserName('firefox')
+
+
+# login class to automate the login process
+class login():
+    driver.maximize_window()
+    def AdManager(self,username,password):
+        driver.find_element_by_name('username').send_keys(username)
+        driver.find_element_by_name('password').send_keys(password)
+        driver.find_element_by_id('btn-signin-signIn').click()
+
+def links():
+    links=driver.find_elements(By.TAG_NAME, "a")
+    print('total links on page',len(links))
+    for link in links:
+        print(link.text)
+    
+
+links()
+l=login()
+l.AdManager('surender.pal@groundtruth.com','Surenderpal@1991')
+sleep(5)
